28054bf2144f70581fc74baed205925c69f515d0
Saketh264 <sakethvemulapati3233@gmail.com>
2026-07-16 20:29:45 +0530

    Time: 207 ms (56.29%) | Memory: 33.6 MB (55.09%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.py
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.py
@@ -4,12 +4,10 @@ class Solution:
         op=[]
         for i in range(len(nums)):
             maxi=max(maxi,nums[i])
-            gcd=math.gcd(maxi,nums[i])
-            op.append(gcd)
+            op.append(math.gcd(maxi,nums[i]))
         op.sort()
         m=len(op)
         sums=0
-        final_gcd=float('-inf')
         for i in range(m//2):
             sums+=math.gcd(op[i],op[m-i-1])
         return sums
