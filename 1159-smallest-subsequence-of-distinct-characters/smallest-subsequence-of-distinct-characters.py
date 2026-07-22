2da7c30feb0e4fd40377c9c7b4b90147f37bd602
Saketh264 <sakethvemulapati3233@gmail.com>
2026-07-22 19:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.71%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        last={}
+        seen=set()
+        for i,ch in enumerate(s):
+            last[ch]=i
+        stack=[]
+        for i,ch in enumerate(s):
+            if ch in seen: continue
+            while stack and stack[-1]>ch and last[stack[-1]]>i:seen.remove(stack.pop())
+            stack.append(ch)
+            seen.add(ch)
+        return ''.join(stack)
