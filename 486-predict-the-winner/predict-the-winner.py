d0a5e2e8a9a58b60e70e38bb2022e99a6675536d
Saketh264 <sakethvemulapati3233@gmail.com>
2026-08-01 19:39:33 +0530

    Time: 1619 ms (15.20%) | Memory: 19.3 MB (79.49%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,8 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def dp(i,j):
+            if i==j: return nums[i]
+            left=nums[i]-dp(i+1,j)
+            right=nums[j]-dp(i,j-1)
+            return max(left,right)
+        return dp(0,len(nums)-1)>=0
