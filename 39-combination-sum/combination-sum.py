e37e08f210e44ba032203e84468b6647946e3156
Saketh264 <sakethvemulapati3233@gmail.com>
2026-08-10 09:26:32 +0530

    Time: 3 ms (95.56%) | Memory: 19.7 MB (33.04%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,14 +3,13 @@ class Solution:
         res=[]
         candidates.sort()
         def comb(ind,target,stack):
-            if ind==len(candidates):
-                if target==0:
-                    res.append(stack.copy())
+            if target==0:
+                res.append(stack.copy())
                 return
-            if candidates[ind]<=target:
-                stack.append(candidates[ind])
-                comb(ind,target-candidates[ind],stack)
-                stack.pop()
+            if ind==len(candidates) or candidates[ind]>target: return 
+            stack.append(candidates[ind])
+            comb(ind,target-candidates[ind],stack)
+            stack.pop()
             comb(ind+1,target,stack)
         comb(0,target,[])
         return res
