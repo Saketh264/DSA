8fc622cba9a586f2f2f100293b89d2b5d5a6695c
Saketh264 <sakethvemulapati3233@gmail.com>
2026-08-21 19:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (60.91%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.py
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.py
@@ -2,12 +2,9 @@ class Solution:
     def resultArray(self, nums: List[int]) -> List[int]:
         arr1=[nums[0]]
         arr2=[nums[1]]
-        op=nums[::-1]
-        op.pop()
-        op.pop()
-        while op:
+        for i in range(2,len(nums)):
             if arr1[-1]>arr2[-1]:
-                arr1.append(op.pop())
+                arr1.append(nums[i])
             else:
-                arr2.append(op.pop())
+                arr2.append(nums[i])
         return arr1+arr2
